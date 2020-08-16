b22f91a3b78350cd530244268f4fc7bbb89eb791
philipjpark <66290921+philipjpark@users.noreply.github.com>
2020-08-16 17:48:09 -0400

    Secret password completed; still need to finish bonus section

---
+++ index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const {promisify} = require('util');
+const readFileAsync = promisify(fs.readFile);
+
+const mostFrequentWord = (text) => {
+  const words = text.toLowerCase().match(/[^_\W]+/g);
+  const tally = {};
+  let mostFrequentWord = null;
+
+  words.forEach(word => {
+    tally[word] = (tally[word] || 0) + 1 ;
+    if(!tally[mostFrequentWord] || tally[word] > tally[mostFrequentWord])
+      mostFrequentWord = word;
+  });
+  return mostFrequentWord;
+}
+
+const findPassword = async () => {
+
+  const poem1 = await readFileAsync("poems/starting-poem.txt", "utf-8")
+  // console.log(mostFrequentWord(poem1))
+  const poem2FileName = await readFileAsync ("poems/happy.txt", "utf-8")
+  // console.log(mostFrequentWord(poem2FileName))
+  const poem3FileName = await readFileAsync ("poems/the.txt", "utf-8")
+  console.log(mostFrequentWord(poem3FileName))
+}
+
+findPassword();
+
